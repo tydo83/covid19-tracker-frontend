c3a95fbd1ec8731125113e5f767d9f13917d482f
Timothy Do <tydo83@gmail.com>
2021-04-01 14:05:19 -0400

    added home

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -3,13 +3,17 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Navbar from './component/Navbar'
 import SignUp from './component/SignUp'
 import Login from './component/Login'
+import Home from './component/Home'
 
 const MainRouter = (props) => {
     return (
         <Router>
             <Navbar user={props.user} handleUserLogout={props.handleUserLogout} />
             <Switch>
-                
+            <Route exact path="/home"  render={(routerProps) => <Home {...routerProps} user={props.user }/> }/>
+                {/* <Route exact path="/home" 
+                    component={Home}
+                    user={props.user} /> */}
                 <Route exact path="/sign-up" component={SignUp} />
                 <Route exact path="/login" component={Login} />
             </Switch>
--- src/component/Home.js
+++ src/component/Home.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class Home extends Component {
+    state = {
+        data: ""
+    }
+    // console.log(props)
+
+    componentDidMount() {
+        try {
+            let payload = axios.get("https://covid-api.mmediagroup.fr/v1/cases")
+            this.setState({
+                data: payload
+            })
+        } catch(e) {
+            console.log(e)
+        }
+    }
+    
     render() {
         return (
-            <div>
-                Home
+            <div style={{ marginTop: 50, textAlign: "center" }}>
+                <input 
+                    style={{ width:300 }}
+                    name="city"    
+                />
+                <br />
+                <button style={{ margin: 10 }} >
+                    Search
+                </button>
             </div>
         )
     }
