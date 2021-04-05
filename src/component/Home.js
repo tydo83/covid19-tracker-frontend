274e3266df63666b9eff0e912a8babe7d11a1414
Timothy Do <tydo83@gmail.com>
2021-04-04 21:49:07 -0400

    updated search bar and onClick

--- src/component/Home.js
+++ src/component/Home.js
@@ -3,7 +3,9 @@ import axios from "axios";
 export default class App extends Component {
     state = {
         data: [],
-        country: ""
+        country: "",
+        isError: false,
+        errorMessage: "",
     };
 
     async componentDidMount() {
@@ -11,10 +13,11 @@ export default class App extends Component {
             let payload = await axios.get(
                 "https://covid-api.mmediagroup.fr/v1/cases?country=Global"
             );
+
             let countryObj = payload.data.All;
             let newCountryArrayData = [];
             newCountryArrayData.push(countryObj)
-            console.log(newCountryArrayData)
+            console.log(payload.all)
             // for (let key in countryObj) {
             //     newCountryArrayData.push({
             //         countryData: countryObj[key],
@@ -33,7 +36,7 @@ export default class App extends Component {
         return this.state.data.map((item, index) => {
             return (
                 <React.Fragment key={item.countryName}>
-                    <h1> {item.countryName}</h1>
+                    <h1> {this.state.country}</h1>
                     <div>Population: {item.confirmed}</div>
                     <div>Confirmed: {item.confirmed}</div>
                     <div>Recoverd: {item.recovered}</div>
@@ -42,6 +45,35 @@ export default class App extends Component {
             );
         });
     };
+
+    handleOnChange = (event) => {
+        this.setState({
+            country: event.target.value
+        })
+    }
+
+    handleOnClick = async (event) => {
+        if(this.state.country.length === 0) {
+            this.setState({
+                isError: true,
+                errorMessage: "Please enter the country",
+                country: "",
+            })
+            return;
+        }
+        try {
+            let payload = await axios.get(`https://covid-api.mmediagroup.fr/v1/cases?country=${this.state.country}`)
+            let countryObj = payload.data.All;
+            let newCountryArrayData = [];
+            newCountryArrayData.push(countryObj)
+            this.setState({
+                data: newCountryArrayData
+            })
+            console.log(this.state.data)
+        } catch(e) {
+        console.log(e)
+        }
+    }
 
     render() {
         return (
