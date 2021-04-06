068dc993870e011800bb4dfde324f89353e284b2
Timothy Do <tydo83@gmail.com>
2021-04-06 12:16:53 -0400

    updated dropdown

--- src/component/Search.js
+++ src/component/Search.js
@@ -9,7 +9,8 @@ export default class App extends Component {
         country: "",
         isError: false,
         errorMessage: "",
-        countryArray:[]
+        countryArray:[],
+        countryData: null,
     };
 
     async componentDidMount() {
@@ -54,34 +55,37 @@ export default class App extends Component {
     //     });
     // };
 
-    // handleOnChange = (event) => {
-    //     this.setState({
-    //         country: event.target.value
-    //     })
-    // }
+    handleOnChange = (event) => {
+        this.setState({
+            country: event.target.value
+            },
+        )
+    }
 
-    // handleOnClick = async (event) => {
-    //     try {
-    //         this.setState({
-    //             countryArray: newCountryArray.
-    //         })
-            
-    //     } catch (e) {
-    //         console.log(e)
-    //     }
-    // }
+    handleOnClick = async (event) => {
+        try {
+            let foundCountry = this.state.countryArray.find(country => country.country === this.state.country) 
+            this.setState({
+                countryData: foundCountry
+            })       
+        } catch (e) {
+            console.log(e)
+        }
+    }
 
     render() {
         return (
             <div style={{ marginTop: 50, textAlign: "center" }}>
                 <img src={world} style={{width: 500}}/>
                 <br />
-                <label htmlFor="items">Choose a country:</label>
-                <select id="items">
+                <div>
+                <label htmlFor="items" style={{ marginRight: 10 }}>Choose a country: </label>
+                <select id="items" onChange={this.handleOnChange} >
+                    <option value="" selected disabled hidden>Choose here</option>
                     {this.state.countryArray.map(country => 
-                    <option onChange={this.handleOnChange} 
-                            value={country}>
-                                {country.country}
+
+                    <option key={country.country} value={country.country}>
+                        {country.country}
                     </option>)}            
                 </select>
                 <button
@@ -90,8 +94,16 @@ export default class App extends Component {
                 >
                     Search
                 </button>
-                {/* {this.showCountryData()} */}
-            </div>
+                {this.state.countryData !== null ? (
+                    <div>
+                        {console.log(this.state.countryData)}
+                        {this.state.countryData.country}
+                        {this.state.countryData.data.All.deaths}
+
+                    </div>
+                ) : "" }
+                </div>
+                </div>
         )
     }
 }
